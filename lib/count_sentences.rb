3f18cc0edd44bf30726a6960500bb74a1278b4be
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-22 01:24:46 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,15 @@ require 'pry'
 class String
 binding.pry
   def sentence?
-    self.end_with(".")
+    self.end_with? (".")
   end
 
   def question?
-self.end_with("?")
+self.end_with? ("?")
   end
 
   def exclamation?
-self.end_with("!")
+self.end_with? ("!")
   end
 
   def count_sentences
